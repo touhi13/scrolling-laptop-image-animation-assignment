d6f1a3f89891799a6bb85066fb044768c3bb35d7
Touhidun Nabi Sarkar <touhi13@gmail.com>
2023-07-15 00:31:48 +0600

    Update image scrolling functionality

--- main.js
+++ main.js
@@ -27,23 +27,23 @@ function handleScroll() {
   // Get the corresponding image path based on the threshold index
   const imagePath = imageSequence[currentIndex];
 
-  // Apply CSS transition for smooth image change
-  laptopImage.style.transition = 'opacity 0.3s ease-in-out';
-
-  // Update the image source and fade in the new image
-  laptopImage.style.opacity = 0;
-  laptopImage.onload = () => {
-    laptopImage.style.opacity = 1;
-  };
+  // Update the image source based on the current threshold index
   laptopImage.src = imagePath;
 }
 
-// Attach the scroll event listener to the totalScrollHeight area only
-window.addEventListener('scroll', () => {
+let animationFrameId;
+
+// Function to handle scroll events
+function handleScrollEvent() {
   const scrollPosition = window.scrollY;
 
   if (scrollPosition <= totalScrollHeight) {
-    handleScroll();
+    cancelAnimationFrame(animationFrameId);
+    animationFrameId = requestAnimationFrame(handleScroll);
   }
-});
+}
+
+// Attach the scroll event listener to the totalScrollHeight area only
+window.addEventListener('scroll', handleScrollEvent);
+
 
